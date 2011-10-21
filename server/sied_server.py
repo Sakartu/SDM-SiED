d838f79b5c07cba77af1c2c79bcf5579c55f89c6
Peter Wagenaar <sakartu@gmail.com>
2011-10-21 16:06:21 +0200

    added dry_run and some test code

--- server/sied_server.py
+++ server/sied_server.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python
 from SimpleXMLRPCServer import SimpleXMLRPCServer
-from req_handler.request_handler import RequestHandler
+from rpc_handler.handler import SiEDRPCHandler
+from db import db
+import hashlib
+import util.util as util
+
 
 conf = { #config parameters
-
+        'db_location' : '~/.sied/sied.db',
+        'db_conn' : None,
         }
+
+dry_run = True
+debug = True
 
 
 def main():
-    server = SimpleXMLRPCServer(("wlan235233.mobiel.utwente.nl", 8000))
-    server.register_introspection_functions()
-    server.register_function(pow)
-    server.serve_forever()
+    db.initialize(conf)
+    
+    if not dry_run:
+        server = SimpleXMLRPCServer(("wlan235233.mobiel.utwente.nl", 8000))
+        server.register_introspection_functions()
+        server.register_instance(SiEDRPCHandler())
+        server.serve_forever()
+    else:
+        s = SiEDRPCHandler(conf)
+        ctext = util.encrypt(hashlib.sha512('jemoeder').digest(), 'bla')
+        s.testcrypto(ctext)
+
 
 if __name__ == '__main__':
     main()
