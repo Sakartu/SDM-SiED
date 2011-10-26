43f5426f815580d2297c417b556da0045f5a6a2b
Peter Wagenaar <sakartu@gmail.com>
2011-10-26 14:57:28 +0200

    added some more keys, updated script

--- server/keys/genkeys.sh
+++ server/keys/genkeys.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-openssl genrsa -out priv.pem 2048 
-openssl rsa -in priv.pem -pubout -out pub.pem
+# consultant key
+openssl genrsa -out consultant.pem 2048 
+openssl rsa -in consultant.pem -pubout -out consultant.pub.pem
+
+openssl genrsa -out client1.pem 2048 
+openssl rsa -in client1.pem -pubout -out client1.pub.pem
+
+openssl genrsa -out client2.pem 2048 
+openssl rsa -in client2.pem -pubout -out client2.pub.pem
