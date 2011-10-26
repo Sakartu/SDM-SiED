0924e20e0092238552c2ef4314e74291d6e2a902
Peter Wagenaar <sakartu@gmail.com>
2011-10-26 12:25:30 +0200

    added logging and some conf params

--- server/sied_server.py
+++ server/sied_server.py
@@ -3,12 +3,18 @@ from SimpleXMLRPCServer import SimpleXMLRPCServer
 from rpc_handler.handler import SiEDRPCHandler
 from db import db
 import hashlib
+import logging
+import os
+import sys
 import util.util as util
 
 
 conf = { #config parameters
         'db_location' : '~/.sied/sied.db',
         'db_conn' : None,
+        'check_sigs' : False,
+        'debug' : False,
+        #'logfile' : '~/SiED.log',
         }
 
 dry_run = True
@@ -16,6 +22,9 @@ debug = True
 
 
 def main():
+    setup_logging()
+    logger = logging.getLogger()
+    logger.info('bla2')
     db.initialize(conf)
     
     if not dry_run:
@@ -27,6 +36,34 @@ def main():
         s = SiEDRPCHandler(conf)
         ctext = util.encrypt(hashlib.sha512('jemoeder').digest(), 'bla')
         s.testcrypto(ctext)
+
+def setup_logging():
+    #if we're in debugging mode we use loglevel DEBUG, otherwise ERROR              
+    level = None                                                                    
+    if conf['debug']:                                                             
+        level = logging.DEBUG                                                       
+    else:                                                                           
+        level = logging.INFO                                                        
+    format = '%(asctime)s : %(message)s'                                            
+    dateformat = '%d/%m/%Y %H:%M:%S'                                                
+    formatter = logging.Formatter(fmt=format, datefmt=dateformat)            
+    logging.getLogger().setLevel(level)                                      
+
+    #then we initialize the logging functionality                                   
+    if 'logfile' in conf:                                                           
+        path = os.path.expanduser(conf['logfile'])                                  
+
+        if not os.path.exists(os.path.dirname(path)):                               
+            try:                                                                    
+                os.makedirs(os.path.dirname(path))                                  
+            except:                                                                 
+                print('Could nog create logfile or dirs, exitting')              
+                sys.exit(2)                                                      
+        handler = logging.FileHandler(path)
+        handler.setFormatter(formatter)
+        logging.getLogger().addHandler(handler)
+    else:
+        logging.basicConfig(format=format, datefmt=dateformat, level=level)
 
 
 if __name__ == '__main__':
