ac41d31d7762292e46ba5cd7eaac698cacd1e056
Peter Wagenaar <sakartu@gmail.com>
2011-10-27 00:19:22 +0200

    added add_pubkey and fetch_key db methods

--- server/db/db.py
+++ server/db/db.py
@@ -1,10 +1,38 @@
-from util import constants
+from util import constants, util
+import sqlite3
+import os
+import logging
+
+logger = logging.getLogger()
 
 def initialize(conf):
-    pass
+    # setup db
+    logger.info('Setting up database...')
+    db_path = util.preparse_path(conf['db_location'])
+    if not os.path.exists(os.path.dirname(db_path)):
+        os.makedirs(os.path.dirname(db_path))
+    conn = sqlite3.connect(db_path)
+    conf[constants.Conf.DB_CONN] = conn
+
+    # init tables
+    with conn:
+        logger.info('Building tables...')
+        c = conn.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS pubkeys(client_id int, tree_id
+                text, pubkey text)''')
+        c.execute('''CREATE UNIQUE INDEX IF NOT EXISTS pubkey_index ON
+        pubkeys(client_id, tree_id)''')
 
 def add_pubkey(conf, client_id, tree_id, pubkey):
-    with conf[constants.DB_CONN] as conn:
+    with conf[constants.Conf.DB_CONN] as conn:
         c = conn.cursor()
-        c.execute('INSERT INTO pubkeys VALUES (?, ?, ?)', (client_id, tree_id, pubkey))
+        try:
+            c.execute('INSERT INTO pubkeys VALUES (?, ?, ?)', (client_id, tree_id, pubkey))
+        except sqlite3.IntegrityError:
+            logger.warn('Tried to insert pubkey for client {id} twice!'.format(id=client_id))
+
+def fetch_key(conf, client_id, tree_id):
+    with conf[constants.Conf.DB_CONN] as conn:
+        c = conn.cursor()
+        c.execute('SELECT pubkey FROM pubkeys WHERE client_id = ? AND tree_id = ?', client_id, tree_id)
 
