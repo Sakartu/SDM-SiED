4a6e71f91ef54449e06a605d6971a7eade8c94e1
Peter Wagenaar <sakartu@gmail.com>
2011-10-27 00:20:42 +0200

    added add_pubkey testcase

--- server/test_client.py
+++ server/test_client.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python -u
 from xmlrpclib import ServerProxy, Error
 import util.util as util
+from base64 import b64encode, b64decode
 
-server = ServerProxy("http://localhost:8000")
+server = ServerProxy("http://localhost:8000", allow_none=True)
 
 print "Connectivity test:"
 a = 'woei'
@@ -12,6 +13,11 @@ print server.test(a, b)
 print ""
 
 print "add_pubkey test:"
+# build params
 client_id = 1
 tree_id = util.digest(client_id)
-server.add_pubkey(
+consultant_privkey = './keys/consultant.pem'
+client_pubkey = "".join(open('./keys/client1.pub.pem').readlines())
+sig = util.sign(consultant_privkey, True, client_id, tree_id, client_pubkey)
+#call the server
+print server.add_pubkey(b64encode(sig), client_id, b64encode(tree_id), client_pubkey)
