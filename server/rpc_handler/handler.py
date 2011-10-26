b3ac524130a6a6b3849fb261d2d7369bc53dd7c6
Peter Wagenaar <sakartu@gmail.com>
2011-10-27 00:19:51 +0200

    changed method signatures once again, added client_id

--- server/rpc_handler/handler.py
+++ server/rpc_handler/handler.py
@@ -9,40 +9,34 @@ logger = logging.getLogger()
 
 
 class SiEDRPCHandler:
+    checker = SigChecker()
     def __init__(self, conf):
         self.conf = conf
 
     def test(self, a, b):
         print type(a), type(b)
-        print dir(a)
-        print str(a.data)
-        print str(a.encode)
-        print str(a.decode)
         return str(a) + str(b)
 
-    @SigChecker(self.conf)
+    @checker
     def testcrypto(self, ctext):
         key = hashlib.sha512('jemoeder').digest()
         logger.info(repr(util.decrypt(key, ctext)))
 
-    @SigChecker
+    @checker
     def add_pubkey(self, sig, client_id, tree_id, pubkey):
-        #first we check the validity of the query using sig
-        if self.conf['check_sigs'] and not self.__check_sig(sig, pubkey):
-            logger.warn('Received command for which signature doesn\'t match, ignoring!')
-            return
-        #then we add the pubkey to the database
+        logger.info('Adding key for client {id}.'.format(id=client_id))
         db.add_pubkey(self.conf, client_id, tree_id, pubkey)
+        return "Added key for client {id}".format(id=client_id)
 
-    @SigChecker
-    def insert(self, sig, treeID, encrypted_rows):
+    @checker
+    def insert(self, sig, client_id, tree_id, encrypted_rows):
         pass
 
-    @SigChecker
-    def update(self, sig, treeID, pre, value):
+    @checker
+    def update(self, sig, client_id, tree_id, pre, value):
         pass
 
-    @SigChecker
-    def search(self, sig, treeID, query, encrypted_content):
+    @checker
+    def search(self, sig, client_id, tree_id, query, encrypted_content):
         pass
 
