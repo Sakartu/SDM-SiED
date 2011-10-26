bfea7c6fbf362b89829d189c849f53430ff066e1
Peter Wagenaar <sakartu@gmail.com>
2011-10-26 12:24:53 +0200

    implemented add_pubkey

--- server/db/db.py
+++ server/db/db.py
@@ -1,2 +1,10 @@
+from util import constants
+
 def initialize(conf):
     pass
+
+def add_pubkey(conf, client_id, tree_id, pubkey):
+    with conf[constants.DB_CONN] as conn:
+        c = conn.cursor()
+        c.execute('INSERT INTO pubkeys VALUES (?, ?, ?)', (client_id, tree_id, pubkey))
+
