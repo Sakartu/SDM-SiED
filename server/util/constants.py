7023c739734919c43aa2b4f914cf2f1f1df0b151
Peter Wagenaar <sakartu@gmail.com>
2011-10-26 12:25:37 +0200

    added constants for conf

--- server/util/constants.py
+++ server/util/constants.py
@@ -1,6 +1,10 @@
 from M2Crypto import RSA
-SIG_HASH_ALG = 'sha1'
+
+SIG_HASH_ALG = 'sha512'
 SIG_PAD_ALG = RSA.pkcs1_padding
 
 class DB:
     KEY_TABLE = 'keys'
+
+class Conf:
+    DB_CONN = 'db_conn'
