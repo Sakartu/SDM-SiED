9b54515ec42e998a28dc443e6b1ff80abfa16de5
Peter Wagenaar <sakartu@gmail.com>
2011-10-27 12:55:08 +0200

    fixed sig checking for consultant

--- server/util/sig_checker.py
+++ server/util/sig_checker.py
@@ -1,25 +1,40 @@
 import logging
-from util import util
+import util
+import traceback
+from base64 import b64decode
 from db import db
 
 logger = logging.getLogger()
 
 class SigChecker(object):
+    def __init__(self, allowed=[]):
+        self.allowed = allowed
+
     def __call__(self, fun):
         def wrapped_fun(*args):
-            conf = args[0].conf
+            try:
+                conf = args[0].conf #args[0] is self
 
-            if conf['check_sigs']:
-                #check the signature
-                sig = args[1]
-                client_id = args[2]
-                tree_id = args[3]
-                #report if wrong
-                logger.debug('Checking signature for {0}'.format(fun.__name__))
-                key = db.fetch_key(conf, client_id)
-                if utils.check_sign(
-                
+                if conf['check_sigs']:
+                    #check the signature
+                    sig = args[1]
+                    client_id = args[2]
+                    tree_id = args[3]
+                    #report if wrong
+                    logger.debug('Checking signature for {0}'.format(fun.__name__))
 
-            fun(*args)
+                    if self.allowed:
+                        key = "".join(open(conf[self.allowed], 'r').readlines())
+                    else:
+                        key = db.fetch_key(conf, client_id, tree_id)
+
+                    logger.debug('Keys found, continuing...')
+                    if util.check_sign(key, b64decode(sig), False, *args[2:]): 
+                        logger.debug('Signature matched, calling function...')
+                        return fun(*args)
+                    else:
+                        logger.warn('Signature didn\'t match for {0}'.format(fun.__name__))
+            except:
+                traceback.print_exc()
         return wrapped_fun
         
