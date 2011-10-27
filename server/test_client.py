1473985394b8c1429abbaa0e8869547f56e106f8
Peter Wagenaar <sakartu@gmail.com>
2011-10-27 12:54:34 +0200

    added add_pubkey tests that work

--- server/test_client.py
+++ server/test_client.py
@@ -10,14 +10,18 @@ a = 'woei'
 b = 'woeiwoei'
 print "this should produce {0}{1}".format(a, b)
 print server.test(a, b)
-print ""
 
-print "add_pubkey test:"
+print "\nadd_pubkey test:"
 # build params
 client_id = 1
 tree_id = util.digest(client_id)
 consultant_privkey = './keys/consultant.pem'
 client_pubkey = "".join(open('./keys/client1.pub.pem').readlines())
-sig = util.sign(consultant_privkey, True, client_id, tree_id, client_pubkey)
+sig = util.sign(consultant_privkey, True, client_id, b64encode(tree_id), client_pubkey)
 #call the server
+print "Everything setup, calling server.."
 print server.add_pubkey(b64encode(sig), client_id, b64encode(tree_id), client_pubkey)
+
+print "\nadd_pubkey for same client test:"
+print server.add_pubkey(b64encode(sig), client_id, b64encode(tree_id), client_pubkey)
+
