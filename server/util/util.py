d6792acbef5024da915210570d3af07bfef325eb
Peter Wagenaar <sakartu@gmail.com>
2011-10-27 12:55:33 +0200

    added some debugging stuff to signing functions

--- server/util/util.py
+++ server/util/util.py
@@ -47,6 +47,10 @@ def sign(keystring, is_file=False, *data):
     arguments with the given private key string. The key string can either be a
     stringrepresentation of a private key or the path to a .pem file.
     '''
+    #print "Signing data:"
+    #for (i, a) in enumerate(data):
+    #    print '{0}. {1}'.format(i, repr(a))
+    #print "With key: {0}".format(keystring)
     if is_file:
         bio = BIO.openfile(keystring)
     else:
@@ -55,7 +59,9 @@ def sign(keystring, is_file=False, *data):
     signEVP = EVP.load_key_bio(bio)
     signEVP.sign_init()
     signEVP.sign_update(digest(*data))
-    return signEVP.sign_final()
+    sig = signEVP.sign_final()
+    #print "Resulting sig: {0}".format(repr(sig))
+    return sig
 
 def check_sign(keystring, sig, is_file=False, *data):
     '''
@@ -63,6 +69,10 @@ def check_sign(keystring, sig, is_file=False, *data):
     an arbitrary number of arguments. The key string can either be a
     stringrepresentation of a public key or the path to a .pem file
     '''
+    #print "Checking sig {0} for data:".format(repr(sig))
+    #for (i, a) in enumerate(data):
+    #    print '{0}. {1}'.format(i, repr(a))
+    #print "With key: {0}".format(keystring)
     if is_file:
         bio = BIO.openfile(keystring)
     else:
