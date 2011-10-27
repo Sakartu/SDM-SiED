45807a00aa67b58c1b943c66a0ce4b4170850de3
Peter Wagenaar <sakartu@gmail.com>
2011-10-27 12:53:21 +0200

    added db removal for debugging and SameKeyException for add_pubkey

--- server/db/db.py
+++ server/db/db.py
@@ -2,6 +2,7 @@ from util import constants, util
 import sqlite3
 import os
 import logging
+from exceptions import SameKeyException
 
 logger = logging.getLogger()
 
@@ -9,6 +10,14 @@ def initialize(conf):
     # setup db
     logger.info('Setting up database...')
     db_path = util.preparse_path(conf['db_location'])
+
+    #remove db if we're debugging
+    if 'debug' in conf and conf['debug']:
+        try:
+            os.remove(db_path)
+        except:
+            pass
+
     if not os.path.exists(os.path.dirname(db_path)):
         os.makedirs(os.path.dirname(db_path))
     conn = sqlite3.connect(db_path)
@@ -30,6 +39,8 @@ def add_pubkey(conf, client_id, tree_id, pubkey):
             c.execute('INSERT INTO pubkeys VALUES (?, ?, ?)', (client_id, tree_id, pubkey))
         except sqlite3.IntegrityError:
             logger.warn('Tried to insert pubkey for client {id} twice!'.format(id=client_id))
+            raise SameKeyException
+
 
 def fetch_key(conf, client_id, tree_id):
     with conf[constants.Conf.DB_CONN] as conn:
