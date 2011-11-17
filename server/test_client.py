c10c9322b65bb5510fcfb49ed4a2a4aa148c5758
Peter Wagenaar <sakartu@gmail.com>
2011-11-17 12:33:10 +0100

    added different testclasses

--- server/test_client.py
+++ server/test_client.py
@@ -5,7 +5,7 @@ from base64 import b64encode
 import unittest
 
 
-class Tests(unittest.TestCase):
+class KeyTests(unittest.TestCase):
     def setUp(self):
         self.server = ServerProxy("http://localhost:8000", allow_none=True)
         self.consultant_id = 0
@@ -25,7 +25,6 @@ class Tests(unittest.TestCase):
         self.assertEqual(str(a) + str(b), self.server.test(a, b))
 
     def test_pubkey_add(self):
-        #sig = util.sign(self.consultant_privkey, False, "add_pubkey", self.client_id, b64encode(self.tree_id), self.client_pubkey)
         sig = util.sign(self.consultant_privkey, False, "add_pubkey")
         #call the server
         expected = "Added key for client {0}".format(self.client_id)
@@ -33,7 +32,6 @@ class Tests(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_pubkey_add_twice(self):
-        #sig = util.sign(self.consultant_privkey, False, "add_pubkey", self.client_id, b64encode(self.tree_id), self.client_pubkey)
         sig = util.sign(self.consultant_privkey, False, "add_pubkey")
         expected = "Tried to add key for client {0} twice!".format(self.client_id)
         self.server.add_pubkey(b64encode(sig), self.client_id, b64encode(self.tree_id), self.client_pubkey)
@@ -42,7 +40,6 @@ class Tests(unittest.TestCase):
 
     def test_pubkey_del(self):
         #first add the key
-        #sig = util.sign(self.consultant_privkey, False, "add_pubkey", self.client_id, b64encode(self.tree_id), self.client_pubkey)
         sig = util.sign(self.consultant_privkey, False, "add_pubkey")
         self.server.add_pubkey(b64encode(sig), self.client_id, b64encode(self.tree_id), self.client_pubkey)
 
@@ -54,7 +51,6 @@ class Tests(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_pubkey_fetch(self):
-        #sig = util.sign(self.consultant_privkey, False, "add_pubkey", self.client_id, b64encode(self.tree_id), self.client_pubkey)
         sig = util.sign(self.consultant_privkey, False, "add_pubkey")
         self.server.add_pubkey(b64encode(sig), self.client_id, b64encode(self.tree_id), self.client_pubkey)
 
@@ -79,6 +75,25 @@ class Tests(unittest.TestCase):
         sig = util.sign(self.consultant_privkey, False, "clear_keys")
         result = self.server.clear_keys(b64encode(sig))
         self.assertTrue(result)
+
+
+class DBTests(unittest.TestCase):
+    def setUp(self):
+        self.server = ServerProxy("http://localhost:8000", allow_none=True)
+        self.consultant_id = 0
+        self.consultant_privkey = "".join(open('./keys/consultant.pem').readlines())
+        self.consultant_pubkey = "".join(open('./keys/consultant.pub.pem').readlines())
+        self.client_privkey = "".join(open('./keys/client1.pem').readlines())
+        self.client_pubkey = "".join(open('./keys/client1.pub.pem').readlines())
+        self.client_id = 1
+        self.tree_id = util.digest(self.client_id)
+        # add a pubkey to the serv
+        sig = util.sign(self.consultant_privkey, False, "add_pubkey")
+        #call the server
+        self.server.add_pubkey(b64encode(sig), self.client_id, b64encode(self.tree_id), self.client_pubkey)
+
+    def tearDown(self):
+        self.server.clear_db()
 
 if __name__ == '__main__':
     tests = unittest.main()
