59bf6bacf10e4028fc9001a440f975036d1e95d9
Peter Wagenaar <sakartu@gmail.com>
2011-11-20 00:05:30 +0100

    added some more tests

--- server/test.py
+++ server/test.py
@@ -1,7 +1,7 @@
 from hashlib import sha512
 from util import util
 from binascii import hexlify
-from base64 import b64encode
+from base64 import b64encode, b64decode
 
 key = sha512('je_moeder').digest()
 print 'key:', hexlify(key)
@@ -22,3 +22,20 @@ print 'b64(cnopad):', b64encode(cnopad)
 cnopad = util.encrypt(key, 'a'*15)
 print 'hex(cnopad):', hexlify(cnopad)
 print 'b64(cnopad):', b64encode(cnopad)
+
+p = b64decode('lefa8X/OjnWoE2M5Q4fLJw==')
+k = b64decode('lefa8X/OjnWoE2M5Q4fLJw==')
+print "test 1:", b64encode(util.encrypt(k, p))
+p = b64decode('uRxjhhONcQk=')
+k = b64decode('lefa8X/OjnWoE2M5Q4fLJw==')
+print "test 2:", b64encode(util.encrypt(k, p))
+p = 'wzup'
+k = b64decode('lefa8X/OjnWoE2M5Q4fLJw==')
+print "test 3:", b64encode(util.encrypt(k, p))
+p = 'a'*16
+k = b64decode('lefa8X/OjnWoE2M5Q4fLJw==')
+print "test 4:", b64encode(util.encrypt(k, p))
+p = 'price'
+k = b64decode('lefa8X/OjnWoE2M5Q4fLJw==')
+print "test 5:", b64encode(util.encrypt(k, p))
+assert 'price' == util.decrypt(k, util.encrypt(k, p))
